e7284410309b3d1af8251a7d310f15cecb9326ac
Hajhosua <124222545+Hajhosua@users.noreply.github.com>
2023-03-30 17:45:36 -0600

    Add files via upload

---
+++ modulo.js
@@ -0,0 +1,30 @@
+//modulos en javaScrip
+
+/*import {sumar} from './modulo2'
+const saludar =(nombre,apellido)=>{
+    let suma = sumar (5,3);
+    return`hola ${nombre} ${apellido} tienes ${suma} vidas`;
+}
+
+console.log(saludar('juan','perez'))*/
+
+import { nombre,suma } from "./modulo2.js";
+console.log(nombre);
+console.log(suma(1,3))
+/*
+-crear minimo dos modulos
+-una clase que herede de otra 
+-un objeto  que dentro de sus propiedades  tenga otro objeto
+
+-el segundo objeto debe tener
+
+-importacion de clases y el objeto
+-funcion tipo flecha que realice el uso  de la clase  y objeto importados
+
+
+-oros adicionales
+-tercer modulo que contenga:
+-una funcion tipo flecha, que me manipule  arreglos  con los metodos de arreglos 
+-debe de tener como  parámetros  de entrada  el arreglo  y una  callback function 
+-debe poderse exportar
+*/
---
+++ modulo2.js
@@ -0,0 +1,15 @@
+ 
+ //export defaul, significa que por defecto exporta la funcion
+ //la puedo usar en otra parte
+ /*export default  sumar =(numero1,numero2)=>{
+    return numero1+numero2;
+}*/
+
+
+export const nombre ='JUAN PEREZ';
+
+export function suma(n1,n2){
+return n1+n2;
+}
+
+
